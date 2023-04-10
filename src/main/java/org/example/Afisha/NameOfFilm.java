d7480a83b8aa9deb62366ae2e0e70065072beaf5
Arthur Petrosov <arthurpetrosov@gmail.com>
2023-04-10 19:16:45 +0400

    Add Afisha Manager commit

---
+++ src/main/java/org/example/Afisha/NameOfFilm.java
@@ -0,0 +1,10 @@
+package org.example.Afisha;
+
+public class NameOfFilm {
+    private String filmsName;
+
+    public NameOfFilm(String filmsName) {
+        this.filmsName = filmsName;
+    }
+}
+
